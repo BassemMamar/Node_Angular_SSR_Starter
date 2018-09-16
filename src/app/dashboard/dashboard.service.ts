cec7fdc1905e76fc736e90d003e3647e5596e4fc
bassemmamar <bassemmamar@gmail.com>
2018-09-16 16:16:03 +0300

    ..

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -4,16 +4,34 @@ import { catchError } from 'rxjs/operators';
 
 import { HttpErrorHandlingService } from '../core/services/http-error-handling/http-error-handling.service';
 import { LoggerService } from '../core/base/logger/logger.service';
+import { IPost } from './models/posts';
 
 @Injectable()
 export class DashboardService {
-    constructor(private http: HttpClient, private logger: LoggerService, private httpErrorHandlingService: HttpErrorHandlingService) { }
+  private apiBase = 'https://jsonplaceholder.typicode.com/';
+  constructor(
+    private http: HttpClient,
+    private logger: LoggerService,
+    private httpErrorHandlingService: HttpErrorHandlingService
+  ) {}
 
-    getHeros() {
-        return this.http.get('api/heroes')
-            .pipe(
-            catchError(error => this.httpErrorHandlingService.handleAsObservable(error))
-            );
-    }
+  getPosts() {
+    return this.http
+      .get<IPost[]>(this.apiBase + 'posts')
+      .pipe(
+        catchError(error =>
+          this.httpErrorHandlingService.handleAsObservable(error)
+        )
+      );
+  }
 
+  getHeros() {
+    return this.http
+      .get('api/heroes')
+      .pipe(
+        catchError(error =>
+          this.httpErrorHandlingService.handleAsObservable(error)
+        )
+      );
+  }
 }
--- src/app/dashboard/new-home/new-home.component.ts
+++ src/app/dashboard/new-home/new-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggerService } from '../../core/base/logger/logger.service';
+import { DashboardService } from '../dashboard.service';
+import { IPost } from '../models/posts';
 
 @Component({
   selector: 'app-new-home',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-home.component.css']
 })
 export class NewHomeComponent implements OnInit {
-
-  constructor() { }
+  posts: IPost[];
+  constructor(
+    private logger: LoggerService,
+    private dashboardService: DashboardService
+  ) {}
 
   ngOnInit() {
+    this.dashboardService.getPosts().subscribe(x => this.posts = x);
   }
 
+  onClickMe() {
+    this.logger.log('tests');
+  }
 }
